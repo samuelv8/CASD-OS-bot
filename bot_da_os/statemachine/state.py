ab8b402e2ccb2ff7b74ef774cbd5f18ac04e860b
samuelv8 <samuel.vendramini@ga.ita.br>
2020-05-12 15:27:10 -0300

    Created a separate state_machine module (part 2)

--- bot_da_os/statemachine/state.py
+++ bot_da_os/statemachine/state.py
@@ -8,18 +8,3 @@ class State:
 
     def next(self, input):
         assert 0, "next not implemented"
-
-
-# Takes a list of Inputs to move from State to
-# State using a template method.
-class StateMachine:
-    def __init__(self, initialState):
-        self.currentState = initialState
-        self.currentState.run()
-
-    # Template method:
-    def run_all(self, inputs):
-        for i in inputs:
-            print(i)
-            self.currentState = self.currentState.next(i)
-            self.currentState.run()
