70975ae78dfe0195cbc167fc978b2aadc4f8860d
samuelv8 <samuel.vendramini@ga.ita.br>
2020-05-12 14:35:20 -0300

    Repaired some errors (still not working)

--- bot_da_os/chatbot/chat_bot.py
+++ bot_da_os/chatbot/chat_bot.py
@@ -1,5 +1,5 @@
 from bot_da_os.statemachine.state import StateMachine, State
-from bot_da_os.statemachine.person.person_action import PersonAction
+from bot_da_os.statemachine.person import person_action
 import sys
 sys.path += ['../statemachine', '../person']
 
@@ -9,7 +9,7 @@ class Waiting(State):
         print("Waiting: Waiting for request")
 
     def next(self, input):
-        if input == PersonAction.query:  # query should be a PersonAction method/attribute
+        if input == person_action.query:
             return ChatBot.processing
         return ChatBot.waiting
 
@@ -55,4 +55,4 @@ ChatBot.tracking = Tracking()
 
 
 moves = map(str.strip, open("../statemachine/person/person_moves.txt").readlines())
-ChatBot().run_all(map(PersonAction, moves))
+ChatBot().run_all(map(person_action, moves))
