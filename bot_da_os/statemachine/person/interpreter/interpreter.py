7d3100dc996dd892a3622e5fdfe69fa185563931
Vinicius <vinicius.amorim@ga.ita.br>
2020-05-20 22:13:15 -0300

    Small changes in person_interpreter

--- bot_da_os/statemachine/person/interpreter/interpreter.py
+++ bot_da_os/statemachine/person/interpreter/interpreter.py
@@ -1,11 +1,18 @@
 # module to process the natural language
 
-# import re
-# import nltk
-
+import re
+import nltk
+import string
 
 # function will receive a message string and return a tuple (message_type, info)
 def person_interpreter(message: str, state) -> tuple:
     # print(state.__class__.__name__) use this to see the class name
-    ...
+    msg = message.lower()
+    words = nltk.word_tokenize(msg)
+    if state.__class__.__name__ == "Waiting":
+        re.search("", words)
+    elif state.__class__.__name__ == "Processing":
+        re.search("", words)
+    elif state.__class__.__name__ == "Tracking":
+        re.search("", words)
     return message, None
