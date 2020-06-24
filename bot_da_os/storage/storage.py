356495207654e3f160f3297fa24495f9e138dea0
Alex the mega one <alexandre.bernat@ga.ita.br>
2020-06-24 13:02:36 -0300

    updates to db functions (organization). Not integrated yet.

--- bot_da_os/storage/storage.py
+++ bot_da_os/storage/storage.py
@@ -13,9 +13,6 @@ def create_connection(path):
     return connection
 
 
-connection = create_connection("db_orders.db")
-
-
 def execute_query(connect, query):
     cursor = connect.cursor()
     try:
@@ -26,8 +23,8 @@ def execute_query(connect, query):
         print(f"The error '{e}' occurred")
 
 
-def execute_read_query(connection, query):
-    cursor = connection.cursor()
+def execute_read_query(connect, query):
+    cursor = connect.cursor()
     result = None
     try:
         cursor.execute(query)
@@ -37,38 +34,42 @@ def execute_read_query(connection, query):
         print(f"The error '{e}' occurred")
 
 
-create_os_table = """
-CREATE TABLE IF NOT EXISTS db_service_orders (
-  os_position INTEGER IDENTITY, 
-  id_cellphone INTEGER PRIMARY KEY,
-  nome TEXT NOT NULL,
-  ap TEXT,
-  p_room TEXT,
-  p_type TEXT,
-  p_description TEXT,
-  status BIT NOT NULL
-);"""
-execute_query(connection, create_os_table)
-
-create_os_synonyms = """
-CREATE TABLE IF NOT EXISTS db_synonyms (
-  id IDENTITY 
-  intended TXT NOT NULL PRIMARY KEY,
-  given TXT NOT NULL,
-  status BIT NOT NULL
-);"""
-
-def save_information(info: str, nome_da_col: str, id: int):
-    records = "INSERT INTO db_service_orders (id_cellphone, nome_da_col) VALUES (id, info)"
-    execute_query(connection, records)
-
-
-def save_synonym(given_info: str, intended_info: str, yes: bool):
-    records = "INSERT INTO db_synonyms (intended, given, status) VALUES (intended_info, given_info, yes)"
-    execute_query(connection, records)
-
-def show_table (tb):
+def show_table(tb, connect):
     select = "SELECT * FROM tb"
-    lida = execute_read_query(connection, select)
+    lida = execute_read_query(connect, select)
     for linha in lida:
         print(linha)
+
+
+def save_information(info: str, nome_da_col: str, id: int, connect):
+    records = "INSERT INTO db_service_orders (id_cellphone, {nome_da_col}) VALUES ({id}, {info})"
+    execute_query(connect, records)
+
+
+def save_synonym(given_info: str, intended_info: str, yes: bool, connect):
+    records = "INSERT INTO db_synonyms (intended, given, status) VALUES (intended_info, given_info, yes)"
+    execute_query(connect, records)
+
+
+if __name__ == '__main__':
+    create_os_table = """
+    CREATE TABLE IF NOT EXISTS db_service_orders (
+      os_position INTEGER IDENTITY, 
+      id_cellphone INTEGER PRIMARY KEY,
+      nome TEXT NOT NULL,
+      ap TEXT,
+      p_room TEXT,
+      p_type TEXT,
+      p_description TEXT,
+      status BIT NOT NULL
+    );"""
+    execute_query(connection, create_os_table)
+
+    create_os_synonyms = """
+    CREATE TABLE IF NOT EXISTS db_synonyms (
+      id IDENTITY 
+      intended TXT NOT NULL PRIMARY KEY,
+      given TXT NOT NULL,
+      status BIT NOT NULL
+    );"""
+    execute_query(connection, create_os_synonyms)
